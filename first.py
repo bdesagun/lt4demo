35212913fe1a8aa255d886b37924abf0beaca43f
Brian <brian.bert.de.sagun@wdc.com>
2021-07-19 21:16:02 +0800

    print

--- first.py
+++ first.py
@@ -5,4 +5,4 @@ data = pd.read_json('/mnt/data/public/yelp/challenge12/'
                       'yelp_dataset/yelp_academic_dataset_photo.json',
                         lines=True)
 x = data.groupby('business_id')['label'].apply(set)
-x
+print(x)
