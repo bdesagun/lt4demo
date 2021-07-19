ee9fab6c82fdef662a8e8000f9562e2a428746b5
Brian <brian.bert.de.sagun@wdc.com>
2021-07-19 21:13:57 +0800

    chagned

--- first.py
+++ first.py
@@ -1,1 +1,8 @@
 print('hello world!')
+
+import pandas as pd
+data = pd.read_json('/mnt/data/public/yelp/challenge12/'
+                      'yelp_dataset/yelp_academic_dataset_photo.json',
+                        lines=True)
+x = data.groupby('business_id')['label'].apply(set)
+x
